fb74cb448a3d3697dcbf660ee8b1da92a9f477cf
cristianku <c.zantedeschi@gmail.com>
2022-11-27 19:10:40 +0100

    c

--- winder_cnc_lcd/Winder.h
+++ winder_cnc_lcd/Winder.h
@@ -31,38 +31,6 @@ int buttons [2] [4]= { {5,
 
 String desc [2] = { "Run Winder" , "0 completed"};
 
-// void draw(void){
-
-//     for ( uint8_t i = 0; i < ArrayCount(buttons) ; ++i ) {
-
-//       // my_lcd.Fill_Round_Rectangle(buttons[i][0], buttons[i][1], buttons[i][2],buttons[i][3], buttons[i][4]);
-    
-//       // show_string(buttons_desc[i], buttons[i][0] + 20,get_y_text_pos (buttons[i][3] , buttons[i][1],buttons[i][5]) ,buttons[i][5],BLACK, BLACK,1);
-//       if ( i == selected  ){
-
-//               draw_button(desc[i], // uint8_t *desc,
-//                   buttons[i][0],   // int16_t x_from, 
-//                   buttons[i][1],   // int16_t y_from, 
-//                   buttons[i][2],   // int16_t x_to, 
-//                   buttons[i][3],   // int16_t y_to, 
-//                   5,   // int16_t corner_radius, 
-//                   2,   //int16_t text_dimension ,
-//                   3   //int16_t button_color
-//                 );
-//       }else {
-//       draw_button(desc[i], // uint8_t *desc,
-//                   buttons[i][0],   // int16_t x_from, 
-//                   buttons[i][1],   // int16_t y_from, 
-//                   buttons[i][2],   // int16_t x_to, 
-//                   buttons[i][3],   // int16_t y_to, 
-//                   5,   // int16_t corner_radius, 
-//                   2,   //int16_t text_dimension ,
-//                   1    //int16_t button_color
-//                 );
-//       }     
-//     }
-
-// }
 
 void draw(int i, boolean selected){
        if (i == 1){
